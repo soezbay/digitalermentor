1afdd37d833f574f9d98e7d6ef5bae73ebf999d3
soezbay <116098789+soezbay@users.noreply.github.com>
2023-08-03 22:33:56 +0200

    Startseite angepasst Leiste angepasst route hinzugefügt

--- code/src/App.vue
+++ code/src/App.vue
@@ -4,8 +4,8 @@
       <ion-menu content-id="main-content" type="overlay">
         <ion-content>
           <ion-list id="inbox-list">
-            <ion-list-header>Inbox</ion-list-header>
-            <ion-note>hi@ionicframework.com</ion-note>
+            <ion-list-header>Menu</ion-list-header><br>
+            <ion-note>Digitalermentor</ion-note>
 
             <ion-menu-toggle :auto-hide="false" v-for="(p, i) in appPages" :key="i">
               <ion-item @click="selectedIndex = i" router-direction="root" :router-link="p.url" lines="none" :detail="false" class="hydrated" :class="{ selected: selectedIndex === i }">
---
+++ code/src/views/Startseite.vue
@@ -0,0 +1,31 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-buttons slot="start">
+                    <ion-menu-button color="primary"></ion-menu-button>
+                </ion-buttons>
+                Startseite
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content style="text-align: center; padding: 10px;">
+            Hallo, 'Username'!
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+
+export default {
+    components: {
+        IonPage,
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent
+    }
+}
+
+</script>
