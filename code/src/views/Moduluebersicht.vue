2cf0aea2320b2db226cf542ee95aa0b43e8010e3
Marie Merveille Fogueng Maka <mariemerveille.foguengmaka@studmail.w-hs.de>
2023-08-21 12:38:26 +0200

    Code Formatierung

--- code/src/views/Moduluebersicht.vue
+++ code/src/views/Moduluebersicht.vue
@@ -1,16 +1,16 @@
 <template>
   <ion-page>
-      <ion-header>
-          <ion-toolbar>
-            <div class="title-block">
+    <ion-header>
+      <ion-toolbar>
+        <div class="title-block">
           <ion-title>Modulübersicht</ion-title>
         </div>
-          </ion-toolbar>
-      </ion-header>
-      <ion-content>
-        <!-- <ion-searchbar animated="true" placeholder="Suche"></ion-searchbar> -->
-        <!-- Studiengangauswahl aus Datenbank -->
-            <ion-item>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <!-- <ion-searchbar animated="true" placeholder="Suche"></ion-searchbar> -->
+      <!-- Studiengangauswahl aus Datenbank -->
+      <ion-item>
         <ion-select v-model="selectedStudiengang" label="Studiengang" placeholder="Studiengang auswählen">
           <ion-select-option v-for="studiengang in studiengaenge" :key="studiengang.Kuerzel" :value="studiengang.Kuerzel">
             {{ studiengang.Name }}
@@ -18,37 +18,38 @@
         </ion-select>
       </ion-item>
 
-          <!-- Module aus Datenbank -->
+      <!-- Module aus Datenbank -->
 
-          
-        <!-- <ion-grid :fixed="true"> -->
-          <ion-grid>
-          <ion-row id="font" v-for="(semesterModules, semesterIndex) in moduleSemesters" :key="semesterIndex">
-            <ion-list-header v-if="semesterIndex === 6">
-              <ion-list-header id="font">Wahlpflichtmodul: INF</ion-list-header>
+
+      <!-- <ion-grid :fixed="true"> -->
+      <ion-grid>
+        <ion-row id="font" v-for="(semesterModules, semesterIndex) in moduleSemesters" :key="semesterIndex">
+          <ion-list-header v-if="semesterIndex === 6">
+            <ion-list-header id="font">Wahlpflichtmodul: INF</ion-list-header>
+          </ion-list-header>
+          <ion-list-header v-else-if="semesterIndex === 7">
+            <ion-list-header id="font">Wahlpflichtmodule: Lehreinheit</ion-list-header>
+          </ion-list-header>
+          <ion-list-header v-else>
+            <ion-list-header id="font">
+              {{ semesterIndex + 1 }}. Semester
             </ion-list-header>
-            <ion-list-header v-else-if="semesterIndex === 7">
-              <ion-list-header id="font">Wahlpflichtmodule: Lehreinheit</ion-list-header>
-            </ion-list-header>
-                <ion-list-header v-else> 
-                  <ion-list-header id="font"> 
-                   {{ semesterIndex + 1 }}. Semester
-                </ion-list-header>
-                </ion-list-header>
-                <ion-row id="proSemester">
-          <ion-col  class="jedesModul" v-for="(module, index) in semesterModules" :key="index" @click="toggleDescription(semesterIndex, index)">
+          </ion-list-header>
+          <ion-row id="proSemester">
+            <ion-col class="jedesModul" v-for="(module, index) in semesterModules" :key="index"
+              @click="toggleDescription(semesterIndex, index)">
               <ion-label>{{ module.name }}</ion-label>
-            
+
               <!-- <ion-card-content v-if="module.showDescription">{{ module.description }}</ion-card-content> -->
-            
-            
-            <!-- <ion-text v-if="module.showDescription">
+
+
+              <!-- <ion-text v-if="module.showDescription">
                 <a @click="showModuleDetails(module.name)">Modulbeschreibung </a>
                 <a @click="showModuleRating(module.name)"> Bewertung </a>
               </ion-text> -->
 
-          </ion-col>
-        </ion-row>
+            </ion-col>
+          </ion-row>
         </ion-row>
       </ion-grid>
     </ion-content>
@@ -63,7 +64,7 @@ import { IonSearchbar, IonSelectOption, IonSelect, IonContent, IonHeader, IonLis
 // import { searchCircle } from 'ionicons/icons';
 import axios from 'axios';
 
- 
+
 export default {
   components: {
     IonSearchbar,
@@ -75,15 +76,15 @@ export default {
     IonTitle,
     IonToolbar,
     IonListHeader
-},
+  },
 
   // setup() {
   //     return { searchCircle };
   //   },
 
   name: "ModulUebersicht",
-  data(){
-    return{
+  data() {
+    return {
 
       selectedStudiengang: null,
       studiengaenge: [],
@@ -91,72 +92,72 @@ export default {
       pflichtModule: [],
       moduleSemesters: [
         [
-          {name: "LDS", description: "Logik und diskrete Strukturen (LDS)", showDescription: false},
-          {name: "EPR", description: "Einführung in die Programmierung", showDescription: false},
-          {name: "MGR", description: "Mathematische Grundlagen", showDescription: false},
-          {name: "TGI", description: "Technische Grundlagen der Informatik (TGI)", showDescription: false},
-          {name: "TENI", description: "Technisches Englisch für Informatiker (TENI)", showDescription: false},
+          { name: "LDS", description: "Logik und diskrete Strukturen (LDS)", showDescription: false },
+          { name: "EPR", description: "Einführung in die Programmierung", showDescription: false },
+          { name: "MGR", description: "Mathematische Grundlagen", showDescription: false },
+          { name: "TGI", description: "Technische Grundlagen der Informatik (TGI)", showDescription: false },
+          { name: "TENI", description: "Technisches Englisch für Informatiker (TENI)", showDescription: false },
         ],
         [
-          {name: "ADS", description: "Algorithmen und Datenstrukturen", showDescription: false},
-          {name: "OPR", description: "Objektorientierte Programmierung", showDescription: false},
-          {name: "SLA", description: "Statistik und Lineare Algebra", showDescription: false},
-          {name: "THI", description: "Theoretische Informatik (THI)", showDescription: false},
-          {name: "BSY", description: "Betriebssysteme (BSY)", showDescription: false},
+          { name: "ADS", description: "Algorithmen und Datenstrukturen", showDescription: false },
+          { name: "OPR", description: "Objektorientierte Programmierung", showDescription: false },
+          { name: "SLA", description: "Statistik und Lineare Algebra", showDescription: false },
+          { name: "THI", description: "Theoretische Informatik (THI)", showDescription: false },
+          { name: "BSY", description: "Betriebssysteme (BSY)", showDescription: false },
         ],
         [
-          {name: "DBA", description: "Datenbanksysteme (DBA)", showDescription: false},
-          {name: "SWT", description: "Softwaretechnik (SWT)", showDescription: false},
-          {name: "MCI", description: "Mensch-Computer-Interaktion (MCI)", showDescription: false},
-          {name: "REN", description: "Rechnernetze (REN)", showDescription: false},
-          {name: "INS", description: "Internetsprachen (INS)", showDescription: false},
+          { name: "DBA", description: "Datenbanksysteme (DBA)", showDescription: false },
+          { name: "SWT", description: "Softwaretechnik (SWT)", showDescription: false },
+          { name: "MCI", description: "Mensch-Computer-Interaktion (MCI)", showDescription: false },
+          { name: "REN", description: "Rechnernetze (REN)", showDescription: false },
+          { name: "INS", description: "Internetsprachen (INS)", showDescription: false },
         ],
         [
-          {name: "SPIN", description: "Softwareprojekt Informatik (SPIN)", showDescription: false},
-          {name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "WP: L", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "WP: L", description: "Siehe Auswahl unten", showDescription: false},
+          { name: "SPIN", description: "Softwareprojekt Informatik (SPIN)", showDescription: false },
+          { name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false },
+          { name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false },
+          { name: "WP: L", description: "Siehe Auswahl unten", showDescription: false },
+          { name: "WP: L", description: "Siehe Auswahl unten", showDescription: false },
         ],
         [
-          {name: "SPIN", description: "Softwareprojekt Informatik (SPIN)", showDescription: false},
-          {name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "WP: L", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "WP: L", description: "Siehe Auswahl unten", showDescription: false},
+          { name: "SPIN", description: "Softwareprojekt Informatik (SPIN)", showDescription: false },
+          { name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false },
+          { name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false },
+          { name: "WP: L", description: "Siehe Auswahl unten", showDescription: false },
+          { name: "WP: L", description: "Siehe Auswahl unten", showDescription: false },
         ],
         [
-          {name: "PXP", description: "Praxisphase"},
-          {name: "BA", description: "Bachelorarbeit", showDescription: false},
-          {name: "KBIN", description: "Kolloquium zur Bachelorarbeit Informatik", showDescription: false},
-          ],
-        [
-          {name: "BKV", description: "Betrieb komplexer verteilter Systeme (BKV)", showDescription: false},
-          {name: "BV", description: "Einführung in die Bildverarbeitung (BV)", showDescription: false},
-          {name: "DOW", description: "Data on the Web (DOW)", showDescription: false},
-          {name: "DSP", description: "Data Science in Practice (DSP)", showDescription: false},
-          {name: "EMI", description: "Einführung in die medizinische Informatik (EMI)", showDescription: false},
-          {name: "ERO", description: "Einführung in die Robotik (ERO)", showDescription: false},
-          {name: "INP", description: "Internet-Protokolle (INP)", showDescription: false},
-          {name: "ITS", description: "Grundlagen der IT-Sicherheit (ITS)", showDescription: false},
-          {name: "KBE", description: "Komponentenbasierte Softwareentwicklung (KBE)", showDescription: false},
-          {name: "KI", description: "Künstliche Intelligenz (KI)", showDescription: false},
-          {name: "KNG", description: "Knowledge Graphs (KNG)", showDescription: false},
-          {name: "MCC", description: "Mobile und Cloud Computing (MCC)", showDescription: false},
-          {name: "MRO", description: "Mobile Robotik (MRO)", showDescription: false},
-          {name: "PAP", description: "Parallele Programmierung (PAP)", showDescription: false},
-          {name: "PPR", description: "Prozedurale Programmierung (PPR)", showDescription: false},
-          {name: "PRAX", description: "Practical Security Attacks and Exploitation (PRAX)", showDescription: false},
-          {name: "SWD", description: "Software Design (SWD)", showDescription: false},
+          { name: "PXP", description: "Praxisphase" },
+          { name: "BA", description: "Bachelorarbeit", showDescription: false },
+          { name: "KBIN", description: "Kolloquium zur Bachelorarbeit Informatik", showDescription: false },
         ],
         [
-          {name: "BRW", description: "Betriebliches Rechnungswesen (BRW)", showDescription: false},
-          {name: "DIM", description: "Digitales Marketing (DIM)", showDescription: false},
-          {name: "EBW", description: "Einführung in die Betriebswirtschaftslehre (EBW)", showDescription: false},
-          {name: "GPM", description: "Geschäftsprozessmanagement (GPM)", showDescription: false},
-          {name: "GWI", description: "Grundlagen der Wirtschaftsinformatik (GWI)", showDescription: false},
-          {name: "PMA", description: "Projektmanagement (PMA)", showDescription: false},
-          {name: "PMW", description: "Produktion und Materialwirtschaft (PMW)", showDescription: false},
+          { name: "BKV", description: "Betrieb komplexer verteilter Systeme (BKV)", showDescription: false },
+          { name: "BV", description: "Einführung in die Bildverarbeitung (BV)", showDescription: false },
+          { name: "DOW", description: "Data on the Web (DOW)", showDescription: false },
+          { name: "DSP", description: "Data Science in Practice (DSP)", showDescription: false },
+          { name: "EMI", description: "Einführung in die medizinische Informatik (EMI)", showDescription: false },
+          { name: "ERO", description: "Einführung in die Robotik (ERO)", showDescription: false },
+          { name: "INP", description: "Internet-Protokolle (INP)", showDescription: false },
+          { name: "ITS", description: "Grundlagen der IT-Sicherheit (ITS)", showDescription: false },
+          { name: "KBE", description: "Komponentenbasierte Softwareentwicklung (KBE)", showDescription: false },
+          { name: "KI", description: "Künstliche Intelligenz (KI)", showDescription: false },
+          { name: "KNG", description: "Knowledge Graphs (KNG)", showDescription: false },
+          { name: "MCC", description: "Mobile und Cloud Computing (MCC)", showDescription: false },
+          { name: "MRO", description: "Mobile Robotik (MRO)", showDescription: false },
+          { name: "PAP", description: "Parallele Programmierung (PAP)", showDescription: false },
+          { name: "PPR", description: "Prozedurale Programmierung (PPR)", showDescription: false },
+          { name: "PRAX", description: "Practical Security Attacks and Exploitation (PRAX)", showDescription: false },
+          { name: "SWD", description: "Software Design (SWD)", showDescription: false },
+        ],
+        [
+          { name: "BRW", description: "Betriebliches Rechnungswesen (BRW)", showDescription: false },
+          { name: "DIM", description: "Digitales Marketing (DIM)", showDescription: false },
+          { name: "EBW", description: "Einführung in die Betriebswirtschaftslehre (EBW)", showDescription: false },
+          { name: "GPM", description: "Geschäftsprozessmanagement (GPM)", showDescription: false },
+          { name: "GWI", description: "Grundlagen der Wirtschaftsinformatik (GWI)", showDescription: false },
+          { name: "PMA", description: "Projektmanagement (PMA)", showDescription: false },
+          { name: "PMW", description: "Produktion und Materialwirtschaft (PMW)", showDescription: false },
         ]
       ],
     };
@@ -198,7 +199,6 @@ export default {
 </script>
 
 <style scoped>
-
 .title-block {
   background-color: #8C99004D;
   border-radius: 15px;
@@ -206,51 +206,36 @@ export default {
   text-align: center;
 }
 
- .jedesModul {
- margin: 1%;
+.jedesModul {
+  margin: 1%;
   width: 85px;
   text-align: center;
   border-radius: 15px;
   background: #fff;
   color: #000000;
-  /* font-family: Lorem ipsum;
-  font-size: 16px;
-  font-weight: 400;
-  line-height: 20px;
-  letter-spacing: 0em; */
   text-align: center;
-
-}  
-/*ion-grid {
-    --ion-grid-width: 50%;
-
-    --ion-grid-width-xs: 50%;
-    --ion-grid-width-sm: 288px;
-    --ion-grid-width-md: 384px;
-    --ion-grid-width-lg: 480px;
-    --ion-grid-width-xl: 570px;
-
-    --ion-grid-columns: 2;
-  }*/
-#font{
-font-size: 16px;
-font-weight: 350;
-line-height: 20px;
-letter-spacing: 0em;
-text-align: left;
 
 }
 
-  ion-col {
-    background-color: #135d54;
-    border: solid 3px #fff;
-    color: #fff;
-    text-align: center;
-  }
+#font {
+  font-size: 16px;
+  font-weight: 350;
+  line-height: 20px;
+  letter-spacing: 0em;
+  text-align: left;
 
-#proSemester{
+}
+
+ion-col {
+  background-color: #135d54;
+  border: solid 3px #fff;
+  color: #fff;
+  text-align: center;
+}
+
+#proSemester {
   border-radius: 15px;
-  background: #8C99004D; 
-  
-} 
+  background: #8C99004D;
+
+}
 </style>
