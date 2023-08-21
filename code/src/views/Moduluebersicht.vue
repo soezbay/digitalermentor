dfcc696a269d8310c7dadbc52ff406ebe9a62eda
Marie Merveille Fogueng Maka <mariemerveille.foguengmaka@studmail.w-hs.de>
2023-08-21 11:34:14 +0200

    Modulübersicht in branch Modulübersicht

--- code/src/views/Moduluebersicht.vue
+++ code/src/views/Moduluebersicht.vue
@@ -2,45 +2,53 @@
   <ion-page>
       <ion-header>
           <ion-toolbar>
-              <ion-title><p>Modulübersicht</p></ion-title>
+            <div class="title-block">
+          <ion-title>Modulübersicht</ion-title>
+        </div>
           </ion-toolbar>
       </ion-header>
       <ion-content>
         <!-- <ion-searchbar animated="true" placeholder="Suche"></ion-searchbar> -->
-          <ion-item>
-      <ion-select label="Studiengang" placeholder="Informatik">
-        <ion-select-option value="MIN">Medieninformatik</ion-select-option>
-        <ion-select-option value="WI">Wirtschaftsinformatik</ion-select-option>
-      </ion-select>
-    </ion-item>
+        <!-- Studiengangauswahl aus Datenbank -->
+            <ion-item>
+        <ion-select v-model="selectedStudiengang" label="Studiengang" placeholder="Studiengang auswählen">
+          <ion-select-option v-for="studiengang in studiengaenge" :key="studiengang.Kuerzel" :value="studiengang.Kuerzel">
+            {{ studiengang.Name }}
+          </ion-select-option>
+        </ion-select>
+      </ion-item>
 
-        <ion-grid>
-          <ion-row v-for="(semesterModules, semesterIndex) in moduleSemesters" :key="semesterIndex">
+          <!-- Module aus Datenbank -->
+
+          
+        <!-- <ion-grid :fixed="true"> -->
+          <ion-grid>
+          <ion-row id="font" v-for="(semesterModules, semesterIndex) in moduleSemesters" :key="semesterIndex">
             <ion-list-header v-if="semesterIndex === 6">
-              <ion-list-header><h5>Wahlpflichtmodul: INF</h5></ion-list-header>
+              <ion-list-header id="font">Wahlpflichtmodul: INF</ion-list-header>
             </ion-list-header>
             <ion-list-header v-else-if="semesterIndex === 7">
-              <ion-list-header><h5>Wahlpflichtmodule: Lehreinheit</h5></ion-list-header>
+              <ion-list-header id="font">Wahlpflichtmodule: Lehreinheit</ion-list-header>
             </ion-list-header>
-                <ion-list-header v-else>
-                  <ion-list-header>
-                  <h5>{{ semesterIndex + 1 }}. Semester</h5>
+                <ion-list-header v-else> 
+                  <ion-list-header id="font"> 
+                   {{ semesterIndex + 1 }}. Semester
                 </ion-list-header>
                 </ion-list-header>
-          <ion-col v-for="(module, index) in semesterModules" :key="index" @click="toggleDescription(semesterIndex, index)">
-              <ion-card> 
-                <ion-card-header> 
-                  <ion-card-title>{{ module.name }}</ion-card-title>
-                </ion-card-header>
-              <ion-card-content v-if="module.showDescription">{{ module.description }}</ion-card-content>
-            </ion-card>
+                <ion-row id="proSemester">
+          <ion-col  class="jedesModul" v-for="(module, index) in semesterModules" :key="index" @click="toggleDescription(semesterIndex, index)">
+              <ion-label>{{ module.name }}</ion-label>
             
-            <ion-text v-if="module.showDescription">
+              <!-- <ion-card-content v-if="module.showDescription">{{ module.description }}</ion-card-content> -->
+            
+            
+            <!-- <ion-text v-if="module.showDescription">
                 <a @click="showModuleDetails(module.name)">Modulbeschreibung </a>
                 <a @click="showModuleRating(module.name)"> Bewertung </a>
-              </ion-text>
+              </ion-text> -->
 
           </ion-col>
+        </ion-row>
         </ion-row>
       </ion-grid>
     </ion-content>
@@ -53,6 +61,8 @@
 
 import { IonSearchbar, IonSelectOption, IonSelect, IonContent, IonHeader, IonListHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 // import { searchCircle } from 'ionicons/icons';
+import axios from 'axios';
+
  
 export default {
   components: {
@@ -74,6 +84,11 @@ export default {
   name: "ModulUebersicht",
   data(){
     return{
+
+      selectedStudiengang: null,
+      studiengaenge: [],
+      wahlpflichtModule: [],
+      pflichtModule: [],
       moduleSemesters: [
         [
           {name: "LDS", description: "Logik und diskrete Strukturen (LDS)", showDescription: false},
@@ -98,21 +113,21 @@ export default {
         ],
         [
           {name: "SPIN", description: "Softwareprojekt Informatik (SPIN)", showDescription: false},
-          {name: "Wahlpflicht: INF", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "Wahlpflicht: INF", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "Wahlpflicht: Lehreinheit", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "Wahlpflicht: Lehreinheit", description: "Siehe Auswahl unten", showDescription: false},
+          {name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false},
+          {name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false},
+          {name: "WP: L", description: "Siehe Auswahl unten", showDescription: false},
+          {name: "WP: L", description: "Siehe Auswahl unten", showDescription: false},
         ],
         [
           {name: "SPIN", description: "Softwareprojekt Informatik (SPIN)", showDescription: false},
-          {name: "Wahlpflicht: INF", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "Wahlpflicht: INF", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "Wahlpflicht: Lehreinheit", description: "Siehe Auswahl unten", showDescription: false},
-          {name: "Wahlpflicht: Lehreinheit", description: "Siehe Auswahl unten", showDescription: false},
+          {name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false},
+          {name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false},
+          {name: "WP: L", description: "Siehe Auswahl unten", showDescription: false},
+          {name: "WP: L", description: "Siehe Auswahl unten", showDescription: false},
         ],
         [
           {name: "PXP", description: "Praxisphase"},
-          {name: "Bachelorarbeit", description: "Beschreibung für M1 - Semester 6", showDescription: false},
+          {name: "BA", description: "Bachelorarbeit", showDescription: false},
           {name: "KBIN", description: "Kolloquium zur Bachelorarbeit Informatik", showDescription: false},
           ],
         [
@@ -146,19 +161,96 @@ export default {
       ],
     };
   },
+  mounted() {
+    this.fetchStudiengaenge();
+  },
   methods: {
+    async fetchStudiengaenge() {
+      try {
+        const response = await axios.get('http://localhost:8000/studiengang');
+        this.studiengaenge = response.data.studiengaenge;
+      } catch (error) {
+        console.error('Error fetching studiengaenge:', error);
+      }
+    },
+    async fetchPflichtModule(selectedStudiengang) {
+      try {
+        const response = await axios.get('http://localhost:8000/pflicht/${selectedStudiengang}');
+        this.pflichtModule = response.data.modules
+      } catch (error) {
+        console.error('Error fetching pflichtmodule:', error);
+      }
+    },
+    async fetchWahplfichtModule(selectedStudiengang) {
+      try {
+        const response = await axios.get('http://localhost:8000/studiengang/${selectedStudiengang}');
+        this.wahlpflichtModule = response.data.modules
+      } catch (error) {
+        console.error('Error fetching wahlpfichtmodule:', error);
+      }
+    },
     toggleDescription(semesterIndex, moduleIndex) {
       this.moduleSemesters[semesterIndex][moduleIndex].showDescription = !this.moduleSemesters[semesterIndex][moduleIndex].showDescription;
     },
-    showModuleDetails(moduleName){
-      // Navigation zur Modulbescreibungsseite implementieren
-      //console.log("Modulbeschreibung für", moduleName);
-    },
-    showModuleRating(moduleName){
-      // Navigation zur Bewertungsseite implementieren
-      //console.log("Bewertung für", moduleName);
-    }
   },
 };
 
 </script>
+
+<style scoped>
+
+.title-block {
+  background-color: #8C99004D;
+  border-radius: 15px;
+  padding: 10px;
+  text-align: center;
+}
+
+ .jedesModul {
+ margin: 1%;
+  width: 85px;
+  text-align: center;
+  border-radius: 15px;
+  background: #fff;
+  color: #000000;
+  /* font-family: Lorem ipsum;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0em; */
+  text-align: center;
+
+}  
+/*ion-grid {
+    --ion-grid-width: 50%;
+
+    --ion-grid-width-xs: 50%;
+    --ion-grid-width-sm: 288px;
+    --ion-grid-width-md: 384px;
+    --ion-grid-width-lg: 480px;
+    --ion-grid-width-xl: 570px;
+
+    --ion-grid-columns: 2;
+  }*/
+#font{
+font-size: 16px;
+font-weight: 350;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: left;
+
+}
+
+  ion-col {
+    background-color: #135d54;
+    border: solid 3px #fff;
+    color: #fff;
+    text-align: center;
+  }
+
+#proSemester{
+  border-radius: 15px;
+  background: #8C99004D; 
+  
+} 
+</style>
