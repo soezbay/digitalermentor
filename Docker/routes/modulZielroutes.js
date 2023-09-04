e2dbe171d87b77fe2cad9437317d8e07a8853f20
Adrian Knigge <adrian.knigge@studmail.w-hs.de>
2023-09-04 13:35:49 +0200

    Bei routes Beispiele in den Kommentaren

--- Docker/routes/modulZielroutes.js
+++ Docker/routes/modulZielroutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const modulControllers = require('../controllers/modulZielControllers');
 const router = express.Router();
 
+// Beispiel: localhost:8000/modulZiel/
+//Achtung ist ein Postbefehl
 router.route("/").post(modulControllers.createZiel);
 
+// Beispiel: localhost:8000/modulZiel/Test123
 router.route("/:BenutzerID").get(modulControllers.getAllZieleFromBenutzer);
 
 module.exports = router;
