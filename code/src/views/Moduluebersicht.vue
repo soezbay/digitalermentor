f1bcaffa5910eddad96a10481968d5cf3c3f7070
Adrian Knigge <adrian.knigge@studmail.w-hs.de>
2023-09-11 15:55:15 +0200

    Daten fetch

--- code/src/views/Moduluebersicht.vue
+++ code/src/views/Moduluebersicht.vue
@@ -24,38 +24,21 @@
         </ion-select>
       </ion-item>
 
-      <!-- Module aus Datenbank -->
-
-      <!-- Moduldarstellung-->
+      <!-- Ion Grid für Semester -->
       <ion-grid>
-        <ion-row class="modulfont" v-for="(semesterModules, semesterIndex) in moduleSemesters" :key="semesterIndex">
-          <ion-list-header v-if="semesterIndex === 6">
-            <ion-list-header class="modulfont">Wahlpflichtmodul: INF</ion-list-header>
-          </ion-list-header>
-          <ion-list-header v-else-if="semesterIndex === 7">
-            <ion-list-header class="modulfont">Wahlpflichtmodule: Lehreinheit</ion-list-header>
-          </ion-list-header>
-          <ion-list-header v-else>
-            <ion-list-header class="modulfont">
-              {{ semesterIndex + 1 }}. Semester
-            </ion-list-header>
-          </ion-list-header>
-          <ion-row class="semesterBlock">
-            <ion-col class="modulBlock" v-for="(module, index) in semesterModules" :key="index"
-              @click="openModal(module)">
-              <ion-label>{{ module.name }}</ion-label>
-
-              <!-- Kurze Modulbeschreibung -->
-              <!-- <ion-card-content v-if="module.showDescription">{{ module.description }}</ion-card-content> -->
-
-              <!-- Anzeige Links zur Modulbeschreibungs- und Bewertungsseiten -->
-              <!-- <ion-text v-if="module.showDescription">
-                <a @click="showModuleDetails(module.name)">Modulbeschreibung </a>
-                <a @click="showModuleRating(module.name)"> Bewertung </a>
-                </ion-text> -->
-
-            </ion-col>
-          </ion-row>
+        <ion-row v-for="semester in uniqueSemesters" :key="semester">
+          <ion-col>
+            <ion-label>{{ `Semester ${semester}` }}</ion-label>
+          </ion-col>
+          <ion-col>
+            <!-- Anzeige der ausgewählten Module für das jeweilige Semester -->
+            <ion-list>
+              <ion-item v-for="module in getModulesForSemester(semester)" :key="module.Kuerzel">
+                <ion-label>{{ module.Name }}</ion-label>
+                <ion-note slot="end">{{ module.Leistungspunkte }} LP</ion-note>
+              </ion-item>
+            </ion-list>
+          </ion-col>
         </ion-row>
       </ion-grid>
     </ion-content>
@@ -111,127 +94,102 @@ export default {
   data() {
     return {
 
-      selectedStudiengang: null,
+      selectedStudiengang: "PI",
       studiengaenge: [],
-      wahlpflichtModule: [],
-      pflichtModule: [],
-      moduleSemesters: [
-        [
-          { name: "LDS", description: "Logik und diskrete Strukturen (LDS)", showDescription: false },
-          { name: "EPR", description: "Einführung in die Programmierung", showDescription: false },
-          { name: "MGR", description: "Mathematische Grundlagen", showDescription: false },
-          { name: "TGI", description: "Technische Grundlagen der Informatik (TGI)", showDescription: false },
-          { name: "TENI", description: "Technisches Englisch für Informatiker (TENI)", showDescription: false },
-        ],
-        [
-          { name: "ADS", description: "Algorithmen und Datenstrukturen", showDescription: false },
-          { name: "OPR", description: "Objektorientierte Programmierung", showDescription: false },
-          { name: "SLA", description: "Statistik und Lineare Algebra", showDescription: false },
-          { name: "THI", description: "Theoretische Informatik (THI)", showDescription: false },
-          { name: "BSY", description: "Betriebssysteme (BSY)", showDescription: false },
-        ],
-        [
-          { name: "DBA", description: "Datenbanksysteme (DBA)", showDescription: false },
-          { name: "SWT", description: "Softwaretechnik (SWT)", showDescription: false },
-          { name: "MCI", description: "Mensch-Computer-Interaktion (MCI)", showDescription: false },
-          { name: "REN", description: "Rechnernetze (REN)", showDescription: false },
-          { name: "INS", description: "Internetsprachen (INS)", showDescription: false },
-        ],
-        [
-          { name: "SPIN", description: "Softwareprojekt Informatik (SPIN)", showDescription: false },
-          { name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false },
-          { name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false },
-          { name: "WP: L", description: "Siehe Auswahl unten", showDescription: false },
-          { name: "WP: L", description: "Siehe Auswahl unten", showDescription: false },
-        ],
-        [
-          { name: "SPIN", description: "Softwareprojekt Informatik (SPIN)", showDescription: false },
-          { name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false },
-          { name: "WP: INF", description: "Siehe Auswahl unten", showDescription: false },
-          { name: "WP: L", description: "Siehe Auswahl unten", showDescription: false },
-          { name: "WP: L", description: "Siehe Auswahl unten", showDescription: false },
-        ],
-        [
-          { name: "PXP", description: "Praxisphase" },
-          { name: "BA", description: "Bachelorarbeit", showDescription: false },
-          { name: "KBIN", description: "Kolloquium zur Bachelorarbeit Informatik", showDescription: false },
-        ],
-        [
-          { name: "BKV", description: "Betrieb komplexer verteilter Systeme (BKV)", showDescription: false },
-          { name: "BV", description: "Einführung in die Bildverarbeitung (BV)", showDescription: false },
-          { name: "DOW", description: "Data on the Web (DOW)", showDescription: false },
-          { name: "DSP", description: "Data Science in Practice (DSP)", showDescription: false },
-          { name: "EMI", description: "Einführung in die medizinische Informatik (EMI)", showDescription: false },
-          { name: "ERO", description: "Einführung in die Robotik (ERO)", showDescription: false },
-          { name: "INP", description: "Internet-Protokolle (INP)", showDescription: false },
-          { name: "ITS", description: "Grundlagen der IT-Sicherheit (ITS)", showDescription: false },
-          { name: "KBE", description: "Komponentenbasierte Softwareentwicklung (KBE)", showDescription: false },
-          { name: "KI", description: "Künstliche Intelligenz (KI)", showDescription: false },
-          { name: "KNG", description: "Knowledge Graphs (KNG)", showDescription: false },
-          { name: "MCC", description: "Mobile und Cloud Computing (MCC)", showDescription: false },
-          { name: "MRO", description: "Mobile Robotik (MRO)", showDescription: false },
-          { name: "PAP", description: "Parallele Programmierung (PAP)", showDescription: false },
-          { name: "PPR", description: "Prozedurale Programmierung (PPR)", showDescription: false },
-          { name: "PRAX", description: "Practical Security Attacks and Exploitation (PRAX)", showDescription: false },
-          { name: "SWD", description: "Software Design (SWD)", showDescription: false },
-        ],
-        [
-          { name: "BRW", description: "Betriebliches Rechnungswesen (BRW)", showDescription: false },
-          { name: "DIM", description: "Digitales Marketing (DIM)", showDescription: false },
-          { name: "EBW", description: "Einführung in die Betriebswirtschaftslehre (EBW)", showDescription: false },
-          { name: "GPM", description: "Geschäftsprozessmanagement (GPM)", showDescription: false },
-          { name: "GWI", description: "Grundlagen der Wirtschaftsinformatik (GWI)", showDescription: false },
-          { name: "PMA", description: "Projektmanagement (PMA)", showDescription: false },
-          { name: "PMW", description: "Produktion und Materialwirtschaft (PMW)", showDescription: false },
-        ]
-      ],
+      modules: {
+        pflicht: [] // Pflichtmodule initialisieren
+      }
     };
   },
+  computed: {
+  uniqueSemesters() {
+    // Überprüfen, ob this.modules.pflicht verfügbar ist, bevor auf sie zugegriffen wird
+    if (this.modules.pflicht) {
+      const uniqueSemesters = [...new Set(this.modules.pflicht.map(module => module.Semester))];
+      
+      // Sortieren Sie die einzigartigen Semester aufsteigend
+      uniqueSemesters.sort((a, b) => a - b);
+      
+      console.log("Einzigartige Semester:", uniqueSemesters);
+      return uniqueSemesters;
+    } else {
+      return [];
+    }
+  }
+},
+  created() {
+  console.log("Wir sind hier");
+  // this.fetchStudiengaenge();
+  console.log("Studiengang= ", this.selectedStudiengang)
+  this.fetchPflichtModule(this.selectedStudiengang);
+  },
   mounted() {
-    this.fetchStudiengaenge();
+    // this.fetchStudiengaenge();
+  },
+  watch: {
+    selectedStudiengang(newStudiengang) {
+      this.fetchPflichtModule(newStudiengang);
+    },
   },
   methods: {
-    async fetchStudiengaenge() {
-      try {
-        const response = await axios.get('http://localhost:8000/studiengang');
-        this.studiengaenge = response.data.studiengaenge;
-      } catch (error) {
-        console.error('Error fetching studiengaenge:', error);
-      }
-    },
-    async fetchPflichtModule(selectedStudiengang) {
-      try {
-        const response = await axios.get('http://localhost:8000/pflicht/${selectedStudiengang}');
-        this.pflichtModule = response.data.modules
-      } catch (error) {
-        console.error('Error fetching pflichtmodule:', error);
-      }
-    },
-    async fetchWahplfichtModule(selectedStudiengang) {
-      try {
-        const response = await axios.get('http://localhost:8000/studiengang/${selectedStudiengang}');
-        this.wahlpflichtModule = response.data.modules
-      } catch (error) {
-        console.error('Error fetching wahlpfichtmodule:', error);
-      }
-    },
-    toggleDescription(semesterIndex, moduleIndex) {
-      this.moduleSemesters[semesterIndex][moduleIndex].showDescription = !this.moduleSemesters[semesterIndex][moduleIndex].showDescription;
-    },
+    // async fetchStudiengaenge() {
+    //   try {
+    //     const response = await axios.get('http://localhost:8000/studiengang');
+    //     this.studiengaenge = response.data.studiengaenge;
+    //     console.log("Test")
+    //     console.log(this.studiengaenge)
+    //   } catch (error) {
+    //     console.error('Error fetching studiengaenge:', error);
+    //   }
+    // },
+    async fetchPflichtModule() {
+  try {
+    const response = await fetch(`http://localhost:8000/studiengang/pflicht/${this.selectedStudiengang}`);
+    const data = await response.json();
+    console.log(data);
 
-    async openModal(selectedModul) {
-      const modal = await modalController
-        .create({
-          component: Modal,
-          componentProps: {
-            selectedModul: selectedModul,
-          },
-        })
-        .then((modal) => {
-          modal.present();
-        });
-    },
-  },
+    // Überprüfen Sie, ob die Daten in der Antwort vorhanden sind
+    if (data.pflicht) {
+      this.modules.pflicht = data.pflicht;
+      console.log("Pflichtmodule geladen:", this.modules.pflicht);
+    } else {
+      console.error("Fehler beim Laden der Pflichtmodule.");
+    }
+  } catch (error) {
+    console.error("Fehler beim Abrufen der Daten:", error);
+  }
+},
+
+
+    // async fetchWahplfichtModule(selectedStudiengang) {
+    //   try {
+    //     const response = await axios.get('http://localhost:8000/studiengang/${selectedStudiengang}');
+    //     this.wahlpflichtModule = response.data.modules
+    //   } catch (error) {
+    //     console.error('Error fetching wahlpfichtmodule:', error);
+    //   }
+    // },
+    // toggleDescription(semesterIndex, moduleIndex) {
+    //   this.moduleSemesters[semesterIndex][moduleIndex].showDescription = !this.moduleSemesters[semesterIndex][moduleIndex].showDescription;
+    // },
+
+    // async openModal(selectedModul) {
+    //   const modal = await modalController
+    //     .create({
+    //       component: Modal,
+    //       componentProps: {
+    //         selectedModul: selectedModul,
+    //       },
+    //     })
+    //     .then((modal) => {
+    //       modal.present();
+    //     });
+    // },
+    getModulesForSemester(semester) {
+      // Filtern Sie die Pflichtmodule basierend auf dem ausgewählten Semester
+      return this.modules.pflicht.filter((module) => module.Semester === semester);
+    }
+  }
 };
 </script>
 
