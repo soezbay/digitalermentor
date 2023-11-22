f71efc1b8a5ed29fe1b069fd1a8591c1dffce23c
esin <esin.sanli@studmail.w-hs.de>
2023-11-22 12:53:27 +0100

    texts.js Datei angelegt und Goals umgeändert

--- code/src/goals/Goals.vue
+++ code/src/goals/Goals.vue
@@ -9,7 +9,7 @@
 							src="/resources/Logo_DigitalerMentor.svg"></ion-icon>
 					</ion-button>
 				</ion-buttons>
-				<ion-title>Studienziele</ion-title>
+				<ion-title>{{ texts.pageTitle.studienziele }}</ion-title>
 				<ion-buttons slot="end" style="display: flex; align-items: center">
 					<ion-button
 						class="infoButton"
@@ -28,10 +28,10 @@
 				<ion-row>
 					<ion-col size="12" size-md="6" class="col">
 						<div class="goals-container">
-							<ion-label>Deine Ziele</ion-label>
+							<ion-label>{{ texts.ziele.deineZiele }}</ion-label>
 						</div>
 						<div class="semesterHeader">
-							<ion-label class="labelHeader">Sommersemester</ion-label>
+							<ion-label class="labelHeader">{{ texts.studium.sommersemester }}</ion-label>
 							<ion-buttons slot="end">
 								<ion-button
 									style="padding-right: 15px"
@@ -55,7 +55,7 @@
 										<ion-item-option
 											color="success"
 											@click="switchToWS(goal.id)">
-											<ion-label>Nach Unten</ion-label>
+											<ion-label>{{ texts.ziele.nachUnten }}</ion-label>
 										</ion-item-option>
 									</ion-item-options>
 									<!--Displayin Goal-Element properties-->
@@ -100,13 +100,13 @@
 						<div v-else class="ion-padding">
 							<div class="explanatory-text">
 								<ion-label style="font-size: large"
-									>Keine Ziele definiert.</ion-label
+									>{{ texts.ziele.keineZieleDefiniert }}</ion-label
 								>
 							</div>
 						</div>
 
 						<div class="semesterHeader">
-							<ion-label class="labelHeader">Wintersemester</ion-label>
+							<ion-label class="labelHeader">{{ texts.studium.wintersemester }}</ion-label>
 							<ion-buttons slot="end">
 								<ion-button
 									style="padding-right: 15px"
@@ -130,7 +130,7 @@
 										<ion-item-option
 											color="success"
 											@click="switchToSS(goal.id)">
-											<ion-label>Nach Oben</ion-label>
+											<ion-label>{{ texts.ziele.nachOben }}</ion-label>
 										</ion-item-option>
 									</ion-item-options>
 									<!--Displayin Goal-Element properties-->
@@ -175,7 +175,7 @@
 						<div v-else class="ion-padding">
 							<div class="explanatory-text">
 								<ion-label style="font-size: large"
-									>Keine Ziele definiert.</ion-label
+									>{{ texts.ziele.keineZieleDefiniert }}</ion-label
 								>
 							</div>
 						</div>
@@ -190,7 +190,7 @@
 							id="header"
 							detail="true"
 							lines="none">
-							<ion-label class="labelHeader"> Erreichte Ziele </ion-label>
+							<ion-label class="labelHeader">{{ texts.ziele.erreichteZiele }}</ion-label>
 						</ion-item>
 						<ion-item-divider class="spacer"></ion-item-divider>
 						<ion-item
@@ -200,22 +200,22 @@
 							id="header"
 							detail="true"
 							lines="none">
-							<ion-label class="labelHeader"> Gelöschte Ziele </ion-label>
+							<ion-label class="labelHeader">{{ texts.ziele.geloeschteZiele }}</ion-label>
 						</ion-item>
 
 						<ion-item-divider class="spacer"></ion-item-divider>
 
 						<ion-row class="klausuren-title">
 							<ion-label class="underline"
-								>Diese Module musst du noch bestehen:
+								>{{ texts.ziele.zuBestehendeModule }}
 							</ion-label>
 						</ion-row>
 
 						<!-- List of Semester and modules -->
             <ion-grid v-for="(semester, index) in semesterList" :key="index">
               <ion-row>
-                <label v-if="(index + 1 < semesterList.length)"> {{ index + 1 }}. Semester </label>
-                <label v-else-if="(index + 1 === semesterList.length)"> Wahlpflichtmodule </label>
+                <label v-if="(index + 1 < semesterList.length)"> {{ index + 1 }}. {{ texts.studium.semester }} </label>
+                <label v-else-if="(index + 1 === semesterList.length)"> {{ texts.studium.wahlpflichtmodule }} </label>
               </ion-row>
               <ion-row>
                 <ion-col size="2" v-for="(fach, fachIndex) in semester.faecher" :key="fachIndex">
@@ -238,17 +238,17 @@
 				:presenting-element="presentingElement">
 				<ion-header>
 					<ion-toolbar>
-						<ion-title>Erstelle ein Ziel</ion-title>
+						<ion-title>{{ texts.ziele.erstelleZiel }}</ion-title>
 						<ion-buttons slot="end">
 							<ion-button @click="saveGoal" :disabled="!goal_name" color="light"
-								>Speichern</ion-button
+								>{{ texts.allgemein.speichern }}</ion-button
 							>
 						</ion-buttons>
 					</ion-toolbar>
 				</ion-header>
 				<ion-content>
 					<ion-item>
-						<ion-label position="floating">Zielname</ion-label>
+						<ion-label position="floating">{{ texts.ziele.zielname }}</ion-label>
 						<ion-input v-model="goal_name"></ion-input>
 					</ion-item>
 					<ion-item>
@@ -258,15 +258,15 @@
 							placeholder="Semesterseason"
 							value="Sommersemester">
 							<ion-select-option value="Sommersemester"
-								>Sommersemester</ion-select-option
+								>{{ texts.studium.sommersemester }}</ion-select-option
 							>
 							<ion-select-option value="Wintersemester"
-								>Wintersemester</ion-select-option
+								>{{ texts.studium.wintersemester }}</ion-select-option
 							>
 						</ion-select>
 					</ion-item>
 					<ion-item>
-						<ion-label position="floating">Info</ion-label>
+						<ion-label position="floating">{{ texts.allgemein.info }}</ion-label>
 						<ion-input v-model="info"></ion-input>
 					</ion-item>
 				</ion-content>
@@ -279,17 +279,17 @@
 				:presenting-element="presentingElement">
 				<ion-header>
 					<ion-toolbar>
-						<ion-title>Erstelle ein Ziel</ion-title>
+						<ion-title>{{ texts.ziele.erstelleZiel }}</ion-title>
 						<ion-buttons slot="end">
 							<ion-button @click="saveGoal" :disabled="!goal_name" color="light"
-								>Speichern</ion-button
+								>{{ texts.allgemein.speichern }}</ion-button
 							>
 						</ion-buttons>
 					</ion-toolbar>
 				</ion-header>
 				<ion-content>
 					<ion-item>
-						<ion-label position="floating">Zielname</ion-label>
+						<ion-label position="floating">{{ texts.ziele.zielname }}</ion-label>
 						<ion-input v-model="goal_name"></ion-input>
 					</ion-item>
 					<ion-item>
@@ -299,15 +299,15 @@
 							placeholder="Semesterseason"
 							value="Wintersemester">
 							<ion-select-option value="Sommersemester"
-								>Sommersemester</ion-select-option
+								>{{ texts.studium.sommersemester }}</ion-select-option
 							>
 							<ion-select-option value="Wintersemester"
-								>Wintersemester</ion-select-option
+								>{{ texts.studium.wintersemester }}</ion-select-option
 							>
 						</ion-select>
 					</ion-item>
 					<ion-item>
-						<ion-label position="floating">Info</ion-label>
+						<ion-label position="floating">{{ texts.allgemein.info }}</ion-label>
 						<ion-input v-model="info"></ion-input>
 					</ion-item>
 				</ion-content>
@@ -319,30 +319,19 @@
 			trigger="open-info-modal"
 			:presenting-element="presentingElement">
 			<ion-content>
-				<p>Hier kannst du deine Studienziele verwalten und organisieren.</p>
-
+				<p>{{ texts.ziele.erklaerung.p1 }}</p>
 				<p>
-					<strong>Studienziele hinzufügen:</strong> Klicke auf den Plus-Button
-					unten, um neue Ziele hinzuzufügen.
+					<strong>{{ texts.ziele.erklaerung.p2strong1 }}</strong>
+          {{ texts.ziele.erklaerung.p2 }}
 				</p>
-
-				<p><strong>Studienziele bearbeiten:</strong></p>
+				<p><strong>{{ texts.ziele.erklaerung.p3strong1 }}</strong></p>
 				<ul>
-					<li>Swipe nach links, um ein Ziel als "geschafft" zu markieren.</li>
-					<li>
-						Swipe nach rechts, um ein Ziel in ein anderes Semester zu
-						verschieben.
-					</li>
-					<li>Swipe nach links, um ein Ziel zu löschen.</li>
+					<li>{{ texts.ziele.erklaerung.li1 }}</li>
+					<li>{{ texts.ziele.erklaerung.li2 }}</li>
+					<li>{{ texts.ziele.erklaerung.li3 }}</li>
 				</ul>
-
-				<p>
-					DIe Modulübersicht soll dir dabei helfen, deine Ziele leichter
-					festzulegen. Dir werden dort alle Module angezeigt, die du noch
-					bestehen musst, um dein Studium abzuschließen.
-				</p>
-
-				<p>Viel Erfolg!</p>
+				<p>{{ texts.ziele.erklaerung.p4 }}</p>
+				<p>{{ texts.ziele.erklaerung.p5 }}</p>
 			</ion-content>
 		</ion-modal>
 	</ion-page>
@@ -358,6 +347,7 @@ import {
 	sadOutline,
 } from 'ionicons/icons'
 import axios from 'axios'
+import { texts } from "../texts.js";
 
 import {
 	IonPage,
@@ -439,6 +429,7 @@ export default {
 				{ name: '6. Semester' },
 				{ name: 'Wahlpflichtmodule' },
 			],
+      texts,
 		}
 	},
 
---
+++ code/src/texts.js
@@ -0,0 +1,48 @@
+// texts.js
+export const texts = {
+    studium: {
+        semeter: "Semester",
+        module: "Module",
+        modul: "Modul",
+        studiengang: "Studiengang",
+        sommersemester: "Sommersemester",
+        wintersemester: "Wintersemester",
+        wahlpflichtmodule: "Wahlpflichtmodule",
+    },
+
+    allgemein: {
+        speicher: "Speichern",
+        info: "Info",
+    },
+
+    pageTitle: {
+        dashboard: "Dashboard",
+        moduluebersicht: "Modulübersicht",
+        studienverlauf: "Studienverlauf",
+        studienziele: "Studienziele",
+        profil: "Profil",
+        einstellungen: "Einstellungen",
+    },
+    ziele: {
+        zielname: "Zielname",
+        deineZiele: "Deine Ziele",
+        erreichteZiele: "Erreichte Ziele",
+        geloeschteZiele: "Gelöschte Ziele",
+        keineZieleDefiniert: "Keine Ziele definiert",
+        zuBestehendeModule: "Diese Module musst du noch Bestehen",
+        erstelleZiel: "Erstelle ein Ziel",
+        erklaerung: {
+            p1: "Hier kannst du deine Studienziele verwalten und organisieren.",
+            p2strong1:"Studienziele hinzufügen: ",
+            p2: "Klicke auf den Plus-Button unten, um neue Ziele hinzuzufügen.",
+            p3strong1: "Studienziele bearbeiten: ",
+            li1: "Swipe nach links, um ein Ziel als \"geschafft\" zu markieren.",
+            li2: "Swipe nach rechts, um ein Ziel in ein anderes Semester zu verschieben.",
+            li3: "Swipe nach links, um ein Ziel zu löschen",
+            p4: "Die Modulübersicht soll dir dabei helfen, deine Ziele leichter festzulegen. Dir werden dort alle Module angezeigt, die du noch bestehen musst, um dein Studium abzuschließen.",
+            p5: "Viel Erfolg!",
+        },
+        nachUnten: "Nach unten",
+        nachOben: "Nach oben",
+    }
+  };
